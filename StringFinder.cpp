ea1bc610ef41ce2462405c7d80e343e160fa6cbe
VenKrish3 <kv72810@mail.harpercollege.edu>
2024-08-28 15:34:55 -0500

    Create StringFinder.cpp

---
+++ StringFinder.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int search(string,char);
+int search(string,string);
+
+int main()
+{
+	cout << "the letter t is at " << search("the quick brown fox",'t') << endl;
+	cout << "the word fox is at " << search("the quick brown fox","the") << endl;
+}
+
+int search(string sentence, char x)
+{
+	for(int i = 0; i < sentence.length(); i++)
+	{
+		if(sentence[i] == x)
+		{
+			return i;
+		}
+	}
+	return -1;
+};
+
+//set to length of y so that you dont compare sentence of 3 vs y of 2
+
+int search(string sentence, string y)
+{
+	int total = y.length()-1;
+	for(int i = sentence.length()-1; i >= 0; i--)
+	{
+		if(sentence[i] == y[total])
+		{
+			total--;
+			if(total == 0)
+				return i;
+		}
+	}
+	return -1;	
+};
